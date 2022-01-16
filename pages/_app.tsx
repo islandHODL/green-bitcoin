f557d27ee26b818e58dfa1e0b9e7f367024a8159
islandHODL <islandhodl@protonmail.com>
2022-01-16 17:52:07 +0000

    Build on Nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Container, ThemeProvider } from "theme-ui";
+import { theme } from "../utils/theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Container p={20}>
+        <Component {...pageProps} />
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
